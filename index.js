c2bd101183004300dd917442499fdd96c128e61b
Subaru Goto <subaru.dayo@gmail.com>
2023-11-13 14:34:30 +0100

    fix max width

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const shareBtn = document.querySelector('.fa-share')
 const shareWindow = document.querySelector('.share-window')
+const mediaQuery = window.matchMedia('(min-width: 750px)')
+
 
 // Display share window
 shareBtn.addEventListener('click', (event) => {
-  if (shareWindow.style.display === 'none') {
+  // If the window size is laptop
+  if (shareWindow.style.display === 'none'
+   && mediaQuery.matches) {
+    shareWindow.style.display = 'flex'
+  } else if
+  (shareWindow.style.display === 'none') {
     shareWindow.style.display = 'flex'
     shareBtn.style.backgroundColor = 'hsl(214, 17%, 51%)'
     shareBtn.style.color = '#ffff'
-
   }
   else {
     shareWindow.style.display = 'none'
